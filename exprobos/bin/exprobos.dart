9877a2123535b67001bf863086db5cb8d7070fdb
Gustavo M <moura.sa@aluno.ifsp.edu.br>
2025-05-11 13:11:52 -0400

    add: Leitura do terreno

--- exprobos/bin/exprobos.dart
+++ exprobos/bin/exprobos.dart
@@ -1,5 +1,45 @@
-import 'package:exprobos/exprobos.dart' as exprobos;
+import 'dart:ffi';
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${exprobos.calculate()}!');
+void main() {
+  List<int> entradaInicial = entradasIniciais();
+  int R = entradaInicial[0];
+  int C = entradaInicial[1];
+  int N = entradaInicial[2];
+  int E = entradaInicial[3];
+  List<List<String>> terreno = lerTerreno(R, C);
+
+  for(var linha in terreno){
+    print(linha);
+  }
+
+
+}
+
+List<int> entradasIniciais(){
+  String entradas = stdin.readLineSync()!;
+
+  List<int> lstentradas = entradas.split(' ').map((char) => int.parse(char)).toList();
+
+  return lstentradas;
+}
+
+List<List<String>> lerTerreno(int linha, int coluna){
+  String terreno = "";
+  List<String> lstTerreno = <String>[];
+  List<List<String>> terrenoCompleto = [];
+
+  while (linha > 0){
+    terreno = stdin.readLineSync()!;
+
+    if(terreno.length != coluna){ // Verifica se o usuario digitou o terreno corretamente
+      print("O terreno possui ${coluna} colunas, digite novamente");
+    }else{
+      lstTerreno = terreno.split('').toList();
+      terrenoCompleto.add(lstTerreno);
+      linha--;
+    }
+  }
+  
+  return terrenoCompleto;
 }
